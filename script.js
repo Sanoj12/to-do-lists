3ea39c1a6abb329b315905be6ccae8066a09150a
sanoj c sam <sanojcsam123@gmail.com>
2022-09-17 20:55:28 +0530

    start update script.js file

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.addEventListener('click',function(){
-    const form=document.querySelector('list-form')
+    const form=document.querySelector('#ist-form');
+    const input=document.querySelector('#list-input');
+    const list_el=document.querySelector('#tasks');
+
+
+     form.addEventListener('submit',function(e){
+        e.preventDefault();
+
+        const task=input.value
+     })
+
 })
